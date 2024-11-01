ae5b556abc00dcdcbf9704509a91dfe4e53441a5
Guilherme Moreno Thimóteo da Cunha <vovodoido1@gmail.com>
2024-11-01 19:17:08 -0300

    Cors fix

--- src/server.ts
+++ src/server.ts
@@ -11,9 +11,9 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(routes)
-/*app.use(cors({
+app.use(cors({
   origin: '*'
-}))*/
+}))
 
 const startServer = async () => {
   try {
