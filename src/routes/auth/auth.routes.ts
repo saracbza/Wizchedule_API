d56092203387016f39f2a1d3d936a4be20ac1c6a
Uguu <gnu@uguu.com>
2024-11-06 03:00:11 -0300

    Revertendo o projeto para c7e8ae8

--- src/routes/auth/auth.routes.ts
+++ src/routes/auth/auth.routes.ts
@@ -6,6 +6,8 @@ const authRoutes = Router()
 
 authRoutes.post('/register', AuthController.store)
 authRoutes.post('/login', AuthController.login)
+authRoutes.post('/verify', AuthController.verify)
+authRoutes.put('/changePassword', AuthController.change)
 //authRoutes.post('/refresh', AuthController.refresh)
 authRoutes.post('/logout', authMiddleware, AuthController.logout)
 authRoutes.post('/mudarFoto', authMiddleware, AuthController.mudarFoto)
